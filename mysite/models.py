393a0ce74e9866072959a41ee3bb0367e78ac7d9
sebastian-ml <sebastian2549@wp.pl>
2021-03-06 16:32:56 +0100

    Create basic models - player and team
    player and team can be added by admin panel only

--- mysite/models.py
+++ mysite/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class Team(models.Model):
+    name = models.CharField(max_length=40, primary_key=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class Player(models.Model):
+    first_name = models.CharField(max_length=20)
+    middle_name = models.CharField(max_length=20, blank=True)
+    last_name = models.CharField(max_length=20)
+    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f'{self.first_name} {self.middle_name} {self.last_name}' \
+               f' ({self.team.name})'
