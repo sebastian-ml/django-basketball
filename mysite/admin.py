e32cd0ea9d16b3834afda347913df56e4efe2fb6
sebastian-ml <sebastian2549@wp.pl>
2021-03-08 14:40:28 +0100

    Add player statistics model
    It contains game statistics which belong to specific game

--- mysite/admin.py
+++ mysite/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Team, Player
+from .models import Team, Player, PlayerStatistics
 
 
 # Register your models here.
 admin.site.register(Team)
 admin.site.register(Player)
+admin.site.register(PlayerStatistics)
--- mysite/models.py
+++ mysite/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.db.models import Q
 
 
 class Team(models.Model):
@@ -12,8 +13,31 @@ class Player(models.Model):
     first_name = models.CharField(max_length=20)
     middle_name = models.CharField(max_length=20, blank=True)
     last_name = models.CharField(max_length=20)
-    team = models.ForeignKey(Team, on_delete=models.CASCADE)
+    team = models.ForeignKey(
+        Team,
+        on_delete=models.CASCADE,
+    )
 
     def __str__(self):
         return f'{self.first_name} {self.middle_name} {self.last_name}' \
                f' ({self.team.name})'
+
+
+class PlayerStatistics(models.Model):
+    q1_foul = models.IntegerField()
+    q2_foul = models.IntegerField()
+    q3_foul = models.IntegerField()
+    player = models.ForeignKey(
+        Player,
+        on_delete=models.CASCADE
+    )
+    game = models.ForeignKey(
+        'game.Game',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        unique_together = ['player', 'game']
+
+    def __str__(self):
+        return f'{self.player} | {self.game}'
