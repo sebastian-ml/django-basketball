a09a7c683a299a3433111affc741e493150ef2aa
sebastian-ml <sebastian2549@wp.pl>
2021-03-18 17:01:57 +0100

    Add player ranking for specific season

--- mysite/urls.py
+++ mysite/urls.py
@@ -6,18 +6,19 @@ app_name = 'home'
 template_dir = 'mysite/'
 
 urlpatterns = [
-    path(
-        'player/statistics/add',
-        views.PlayerStatsCreate.as_view(
-            template_name=template_dir + 'player-statistics-add.html'
-        ),
-        name='player-stats-add'
-    ),
-    path(
-        'player/rank',
-        views.PlayerStatsList.as_view(
-            template_name=template_dir + 'player-rank.html'
-        ),
-        name='player-rank'
-    ),
+    path('player/statistics/add/',
+         views.PlayerStatsCreate.as_view(
+             template_name=template_dir + 'player-statistics-add.html'
+         ),
+         name='player-stats-add'),
+    path('player/ranking/',
+         views.PlayerStatsList.as_view(
+             template_name=template_dir + 'player-ranking.html'
+         ),
+         name='player-ranking'),
+    path('player/ranking/season=<int:year>/',
+         views.PlayerStatsSeasonList.as_view(
+             template_name=template_dir + 'player-ranking.html'
+         ),
+         name='player-ranking-by-season'),
 ]
--- mysite/views.py
+++ mysite/views.py
@@ -11,19 +11,36 @@ class PlayerStatsCreate(CreateView):
     success_url = reverse_lazy('home:player-stats-add')
 
 
+def get_player_ranking(year=None):
+    """Get player stats from all seasons or from a certain season."""
+    player_stats = PlayerStatistics.objects.all()
+    if year:
+        player_stats = PlayerStatistics.objects.filter(game__season__year=year)
+
+    return player_stats.values('player__id') \
+        .annotate(total_fouls=Sum('q1_foul'),
+                  games_played=Count('q1_foul')) \
+        .order_by('-total_fouls')
+
+
 class PlayerStatsList(ListView):
+    """Show player stats ranking list."""
     model = PlayerStatistics
-    context_object_name = 'player_stats'
 
     # Note: Currently it contains only fouls - will be expanded in the future
     def get_context_data(self, **kwargs):
-        """Perform statistics aggregation for each player from all games."""
         context = super().get_context_data(**kwargs)
+        context['player_ranking'] = get_player_ranking()
 
-        context['player_rank'] = PlayerStatistics.objects\
-            .values('player__id')\
-            .annotate(total_fouls=Sum('q1_foul'),
-                      games_played=Count('q1_foul'))\
-            .order_by('-total_fouls')
+        return context
+
+
+class PlayerStatsSeasonList(ListView):
+    """Show player stats ranking list for a specific season."""
+    model = PlayerStatistics
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['player_ranking'] = get_player_ranking(self.kwargs['year'])
 
         return context
