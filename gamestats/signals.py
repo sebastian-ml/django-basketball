e44335e2d7fcb78610e825c580295cc550c957d1
sebastian-ml <sebastian2549@wp.pl>
2021-05-14 23:09:48 +0200

    Add signal on game update

--- gamestats/signals.py
+++ gamestats/signals.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, pre_save
 from .models import GameStats
 from game.models import Game
 from django.dispatch import receiver
@@ -13,3 +13,18 @@ def create_game_stats(sender, instance, created, **kwargs):
     if created:
         GameStats.objects.create(game=instance, team=instance.team_1)
         GameStats.objects.create(game=instance, team=instance.team_2)
+
+
+@receiver(pre_save, sender=Game)
+def update_game_stats(sender, instance, **kwargs):
+    before_save = Game.objects.get(id=instance.id)
+
+    if before_save.team_2 != instance.team_2:
+        GameStats.objects \
+            .filter(game=instance, team=before_save.team_2) \
+            .update(team=instance.team_2)
+
+    if before_save.team_1 != instance.team_1:
+        GameStats.objects \
+            .filter(game=instance, team=before_save.team_1) \
+            .update(team=instance.team_1)
